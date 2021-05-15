98df7e031873ee966468fce4075109290f33c27e
Salman <mohdsalmanp866@gmail.com>
2021-05-16 01:11:21 +0530

    Day2

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to 30 days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
